f17a6eed6d6f9ad46acef00fc0385923d5c390c5
Your Name <you@example.com>
2020-03-17 23:14:57 -0400

    asd

--- dates.sh
+++ dates.sh
@@ -3,7 +3,7 @@
 while read date
 do
     fileName=`echo "$date" | tr " " "_"`
-    date="$date 14:00 2016 +0500"
+    date="$date 14:00 2020 +0500"
     echo "Creating file... $fileName"
     touch "$fileName"
     git add "$fileName"
